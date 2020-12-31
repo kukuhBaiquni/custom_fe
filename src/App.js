a3192c9324c8c87fd035ce1960f30e8d681a422f
kukuh baiquni <kukuh.baiquni@gmail.com>
2020-12-31 15:58:53 +0700

    fix eslint

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="App">
-      wooow
-    </div>
-  );
-}
-
-export default App;
---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable arrow-body-style */
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
