cd8783e9a050eb30497afa124c02c98d55b79dde
kukuh baiquni <kukuh.baiquni@gmail.com>
2021-01-19 16:44:49 +0700

    revision lib

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,4 @@
-/* eslint-disabled no-unused-var */
-
-import { style, typography } from '@material-ui/system'
-
 function Home() {
-  /* eslint-disable-next-line no-console */
-  console.log(style, typography)
-
   return (
     <div>
       <h1>Home</h1>
